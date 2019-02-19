2d475a665711b97a3adcd34f32c2cdc2f203511c
Saad Guessous <guessous_saad@hotmail.com>
2019-02-19 08:08:59 +0200

    added D2, exercise 3 solution

--- src/intro_to_clojure/core.clj
+++ src/intro_to_clojure/core.clj
@@ -260,10 +260,6 @@
   (bake-pan 30)
   (cool-pan))
 
-(defn -main []
-  (bake-cake)
-  (bake-cookies))
-
 (def pantry-ingredients #{:flour :sugar})
 
 (defn from-pantry? [ingredient]
@@ -295,3 +291,52 @@
 (defn simple? [ingredient]
   (contains? simple-ingredient ingredient))
 
+(defn fetch-from-pantry
+  ([ingredient]
+   (fetch-from-pantry ingredient 1))
+  ([ingredient amount]
+   (if (from-pantry? ingredient)
+     (do
+       (go-to :pantry)
+       (dotimes [i amount]
+         (load-up ingredient))
+       (go-to :prep-area)
+       (dotimes [i amount]
+         (unload ingredient)))
+     (error "This function only works on ingredients that are stored in the pantry. You asked me to fetch" ingredient))))
+
+(defn fetch-from-fridge
+  ([ingredient]
+   (fetch-from-fridge ingredient 1))
+  ([ingredient amount]
+   (if (from-fridge? ingredient)
+     (do
+       (go-to :fridge)
+       (dotimes [i amount]
+         (load-up ingredient))
+       (go-to :prep-area)
+       (dotimes [i amount]
+         (unload ingredient)))
+     (error "This function only works on ingredients that are stored in the fridge. You asked me to fetch" ingredient))))
+
+;; D2 - Ex 3
+;; Write a function fetch-ingredient which takes the name of an ingredient and an amount and fetches that amount of the ingredient.
+;; Also make it accept just the ingredient, with a default amount of 1.
+(defn fetch-ingredient
+  ([ingredient]
+   (fetch-ingredient ingredient 1))
+  ([ingredient amount]
+   (cond
+     (from-fridge? ingredient)
+     (fetch-from-fridge ingredient amount)
+     (from-pantry? ingredient)
+     (fetch-from-pantry ingredient amount)
+     :else
+     (error "Unknown ingredient" ingredient))))
+
+(defn -main []
+  (fetch-ingredient :flour 12)
+  (fetch-ingredient :egg 65)
+  (fetch-ingredient :milk 48)
+  (fetch-ingredient :sugar 35)
+  (status))
