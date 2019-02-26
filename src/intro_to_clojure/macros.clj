6e51f3306e6966411860c4f6a7ca16ef194b9bc3
Saad Guessous <guessous_saad@hotmail.com>
2019-02-26 20:34:45 +0200

    added macro expansion example

--- src/intro_to_clojure/macros.clj
+++ src/intro_to_clojure/macros.clj
@@ -8,3 +8,14 @@
 (square (rand-int 10))
 
 (macroexpand-1 `(square 10))
+
+;; Expanding a macro
+(defmacro square [x]
+  `(let [x# ~x]
+     (when (number? x#)
+       (* x# x#))))
+
+(macroexpand-1 `(square (rand-int 10)))
+(macroexpand `(square (rand-int 10)))
+(clojure.walk/macroexpand-all `(square (rand-int 10)))
+
