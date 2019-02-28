90cd67bfdbd1771ad26686742f0c118a258da17a
Saad Guessous <guessous_saad@hotmail.com>
2019-02-28 21:06:21 +0200

    added combinators functions partial + complement

--- src/intro_to_clojure/combinators.clj
+++ src/intro_to_clojure/combinators.clj
@@ -16,3 +16,26 @@
 ((constantly* 100))
 (map (constantly "hello") (range 20))
 
+;; complement
+(def non-pos? (complement pos?))
+(non-pos? 0)
+
+(def non-neg? (complement neg?))
+(non-neg? 10) 
+
+(defn complement* [f]
+  (fn [& args]
+    (not (apply f args))))
+
+;; partial
+(def add3 (partial + 3))
+
+(add3 10 10)
+
+(def one-over (partial / 1))
+(one-over 2)
+
+(defn partial* [f & xs]
+  (fn [& ys]
+    (apply f (concat xs ys))))
+
